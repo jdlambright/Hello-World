7d7fdbc0a8a53096056b383476d1919fe1723f22
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-05-27 00:28:49 -0400

    added mario project

--- mario.py
+++ mario.py
@@ -1,1 +1,9 @@
-print(hello)
+while True:
+    print("height: ", end="")
+    n = int(input())
+    if n > 0 and n < 9:
+        break
+
+for i in range(n):
+    print(' ' * (n-1-i), end="")
+    print(('#' * (i+1)) + '  ' + ('#' * (i+1)))
