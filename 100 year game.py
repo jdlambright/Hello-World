a98b5680c38a6de2a17a914ecfa59fdd7f1a258d
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-05-29 18:45:56 -0400

    added mario project

--- 100 year game.py
+++ 100 year game.py
@@ -1,6 +1,5 @@
 name = input("what is you name? ")
-age = int(input("How old are you? "))
-difference = int(100 - age)
-hundred = int(difference + 2020)
+birthyear = int(input("what year were you born? "))
+hundred = birthyear + 100
 
-print("hello " + name + "! You will turn 100 in the year" + hundred + "!")
+print("hello " +str(name) + "! You will turn 100 in the year " + str(hundred) + "!")
