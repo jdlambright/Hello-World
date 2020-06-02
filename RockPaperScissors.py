360562aba1f2c9f12fe26a0b8b43a215414d8103
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-06-01 23:40:44 -0400

    added mario project

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,8 +1,8 @@
 from question import Question
 
 question_prompts = [
-    "what kind of dog is Nolee? \na) A wolf \nb) A chiwahwa \nc) A golden doodle\n\n"
-    "what is Nora's favorite animal? \na) A wolf \nb) A unicorn \nc) A pig\n\n"
+    "what kind of dog is Nolee? \na) A wolf \nb) A chiwahwa \nc) A golden doodle\n\n",
+    "what is Nora's favorite animal? \na) A wolf \nb) A unicorn \nc) A pig\n\n",
     "what is Owen's favorite animal? \na) A dinosaur \nb) A cow \nc) A walrus\n\n"
 ]
 
@@ -18,10 +18,10 @@ def run_test(questions):
         answer = input(question.prompt)
         if answer == question.answer:
             score += 1
-    print("You got " + str(score) + "out of " + str(len(questions)) + " correct!!!")
+    print("You got " + str(score) + " out of " + str(len(questions)) + " correct!!!")
     if score > 1:
         print("great job!")
     else:
-        print("better luck next time!!")
+        print("Better luck next time!!")
 
 run_test(questions)
