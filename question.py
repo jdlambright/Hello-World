b9b6151ec80db8d6d2c20ece19a5c3db810317aa
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-06-05 00:54:51 -0400

    added mario project

--- question.py
+++ question.py
@@ -1,9 +1,2 @@
-class kids:
-    def __init__ (self, name, age, color, food):
-        self.name = name
-        self.age = age
-        self.color = color
-        self.food = food
-
-
-
+def multiply(a, b):
+    return a * b
