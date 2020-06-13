c7c951ebe2107949db314b88754f58232bde4a65
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-06-12 23:57:57 -0400

    added mario project

--- question.py
+++ question.py
@@ -2,11 +2,11 @@ pos = -1
 
 def search(list, n):
     i = 0
-    while i < len(list):
+    for i in range(len(list)):
         if list[i] == n:
             globals()['pos'] = i
             return True
-        i += 1
+
     return False
 
 list = [5,1,4,3,9,7]
