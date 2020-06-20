2505e12981c15a33ddf120b578fddca090bffcaf
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-06-20 00:40:01 -0400

    added mario project

--- Practice.py
+++ Practice.py
@@ -2,12 +2,21 @@
 pos = -1
 
 def search(list, n):
-    i = 0
-    for i in range(len(list)):
-        if list[i] == n:
-            globals()['pos']=i
-            return True
-    return False
+   l = 0
+   u = len(list)-1
+   while l <= u:
+       mid = (l + u)//2
+
+       if list[mid] == n:
+           globals()['pos'] = mid
+           return True
+       else:
+           if list[mid]< n:
+               l = mid + 1
+           else:
+               u = mid +1
+   return False
+
 
 list = [2,4,6,8,15,18]
 
