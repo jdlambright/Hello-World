69a7473ac32c74f22f03e24dc5db98de3a4fb36b
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-06-19 23:56:58 -0400

    added mario project

--- Practice.py
+++ Practice.py
@@ -1,9 +1,19 @@
 #when I learn something new, this is where i go back to practice it on my own
-num1 = 50
-num2 = 30
-result = num1 * num2
+pos = -1
 
-print(result)
+def search(list, n):
+    i = 0
+    for i in range(len(list)):
+        if list[i] == n:
+            globals()['pos']=i
+            return True
+    return False
 
-if result > 1000:
-    print(num1 +num2)
+list = [2,4,6,8,15,18]
+
+n = int(input("enter a number: "))
+
+if search(list, n):
+    print("found at", pos + 1)
+else:
+    print("not found")
