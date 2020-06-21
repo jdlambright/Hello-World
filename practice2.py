8d3fa14c6da53ab8ba60f7810b2c04db7ee05ede
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-06-21 00:21:21 -0400

    added mario project

--- practice2.py
+++ practice2.py
@@ -1,2 +1,19 @@
 # i feel like there was a bug in my other program but could not find it
 # i made this file so I could leave the other and look at it with greg
+pos = -1
+
+def search(list, n):
+    i = 0
+    for i in range(len(list)):
+        if list[i] == n:
+            globals()['pos'] = i
+            return True
+    return False
+
+list = [ 2,4,6,7,9,10]
+n = int(input("enter a number: "))
+
+if search(list,n):
+    print("found at", pos +1)
+else:
+    print("not found")
