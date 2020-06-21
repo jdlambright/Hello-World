ebf3e94715cc72269603e667cdaafde85a5171e7
jdlambright <64182772+jdlambright@users.noreply.github.com>
2020-06-21 00:38:21 -0400

    added mario project

--- practice2.py
+++ practice2.py
@@ -3,17 +3,26 @@
 pos = -1
 
 def search(list, n):
-    i = 0
-    for i in range(len(list)):
-        if list[i] == n:
-            globals()['pos'] = i
+    l = 0
+    u = len(list) - 1
+
+    while l <= u:
+        mid = (l + u)// 2
+
+        if list[mid] == n:
+            globals()['pos'] = mid
             return True
+        else:
+            if list[mid] < n:
+                l = mid + 1
+            else:
+                u = mid + 1
     return False
 
-list = [ 2,4,6,7,9,10]
-n = int(input("enter a number: "))
+list = [2,3,5,6,8,9,15]
+n = int(input("Enter a number: "))
 
-if search(list,n):
-    print("found at", pos +1)
+if search(list, n):
+    print("found at ", pos+1)
 else:
     print("not found")
