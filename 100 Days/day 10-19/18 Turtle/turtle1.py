0b2d2d1e80b8665fd2ee5e32645b6909ebf83981
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-03-06 11:54:11 -0500

    started new class

---
+++ 100 Days/day 10-19/18 Turtle/turtle1.py
@@ -0,0 +1,70 @@
+#####Turtle Intro######
+
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("green")
+
+# for _ in range(5):
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.penup()
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.pendown()
+
+
+# for _ in range(4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+
+
+# def square():
+#     turns = 0
+#     while turns < 4:
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(90)
+#         turns += 1
+# square()
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
+           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+# def draw_shape(sides):
+#     angle = 360/sides
+#     for _ in range(sides):
+#             tim.forward(100)
+#             tim.right(angle)
+#
+# for shape_side in range(3, 11):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+# import turtle as t
+#
+# timmy_the_turtle = t.Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.backward(200)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.left(180)
+# timmy_the_turtle.setheading(0)
+
+
+######## Challenge 1 - Draw a Square ############
