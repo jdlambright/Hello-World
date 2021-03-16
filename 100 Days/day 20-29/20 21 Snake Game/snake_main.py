8ebfdd8db2375031b095c82ad70e9dfb302b7f7f
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-03-15 23:57:50 -0400

    started snake game

--- 100 Days/day 20-29/20 21 Snake Game/snake_main.py
+++ 100 Days/day 20-29/20 21 Snake Game/snake_main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,7 +11,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-#food = Food()
+food = Food()
+scoreboard= Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,8 +24,24 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.keep_score()
+
+    #detect collision with wall
+    if snake.head.xcor() > 285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
