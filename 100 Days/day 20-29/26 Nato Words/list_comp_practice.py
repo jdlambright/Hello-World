01efba61db9608cb652f71a44f36e7425a7168c9
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-03-30 13:43:52 -0400

    practiced list and dictionary comprehension

--- 100 Days/day 20-29/26 Nato Words/list_comp_practice.py
+++ 100 Days/day 20-29/26 Nato Words/list_comp_practice.py
@@ -42,6 +42,17 @@ new_dict = {new_key: new_value for (key,value) in dict.items() if test}
 
 passed_students = {student: score for (student, score) in students_scores.items() if score >= 60}
 
+#data frames
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
 
+#loop through column of a data frame
+for (key, value) in student_data_frame.items():
+    print(key)
+
+#loop through rows of a data frame
+for(index,row) in student_data_frame.iterrows():
+    print(row)
 
 
