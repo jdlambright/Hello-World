37e7462ee1be3103cfd6c66a9fdbb0cf37800cd3
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-04-01 21:01:50 -0400

    started using tkinter

--- 100 Days/day 20-29/27 TKinter/tkinter_notes.py
+++ 100 Days/day 20-29/27 TKinter/tkinter_notes.py
@@ -75,8 +75,8 @@ def radio_used():
     print(radio_state.get())
 #Variable to hold on to which radio button value is checked.
 radio_state = IntVar()
-radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
-radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1 = Radiobutton(text="yes", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="no", value=2, variable=radio_state, command=radio_used)
 radiobutton1.pack()
 radiobutton2.pack()
 
